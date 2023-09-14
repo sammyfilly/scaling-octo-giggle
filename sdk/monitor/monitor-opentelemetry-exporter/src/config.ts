b209a5fc0c632f749ff6500a52c23947c5defc3b
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 05:50:35 +0100

    Initialize changelog-3.17.2

---
+++ sdk/monitor/monitor-opentelemetry-exporter/src/config.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+import { TokenCredential } from "@azure/core-auth";
+import { ServiceApiVersion } from "./Declarations/Constants";
+import { ApplicationInsightsClientOptionalParams } from "./generated";
+
+/**
+ * Provides configuration options for AzureMonitorTraceExporter.
+ */
+export interface AzureMonitorExporterOptions extends ApplicationInsightsClientOptionalParams {
+  /**
+   * Azure Monitor Connection String, if not provided the exporter will try to use environment variable APPLICATIONINSIGHTS_CONNECTION_STRING
+   * Ex: "InstrumentationKey=00000000-0000-0000-0000-000000000000;IngestionEndpoint=https://dc.services.visualstudio.com"
+   */
+  connectionString?: string;
+  /**
+   * Azure service API version.
+   */
+  apiVersion?: ServiceApiVersion;
+  /**
+   * Azure Active Directory Credential
+   */
+  aadTokenCredential?: TokenCredential;
+  /**
+   * Directory to store retriable telemetry when it fails to export.
+   */
+  storageDirectory?: string;
+  /**
+   * Disable offline storage when telemetry cannot be exported.
+   */
+  disableOfflineStorage?: boolean;
+}
