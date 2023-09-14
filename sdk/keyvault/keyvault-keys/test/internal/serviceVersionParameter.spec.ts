6565ef7ca3f7a29920ad445f775caaa128ec3a90
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:07:43 +0100

    Initialize revert-24449-previewcontainerservice

---
+++ sdk/keyvault/keyvault-keys/test/internal/serviceVersionParameter.spec.ts
@@ -0,0 +1,79 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "@azure/test-utils";
+import { SinonSandbox, SinonSpy, createSandbox } from "sinon";
+import { KeyClient } from "../../src";
+import { LATEST_API_VERSION } from "../../src/keysModels";
+import {
+  HttpClient,
+  PipelineRequest,
+  PipelineResponse,
+  createHttpHeaders,
+} from "@azure/core-rest-pipeline";
+import { ClientSecretCredential } from "@azure/identity";
+import { versionsToTest } from "@azure/test-utils";
+import { serviceVersions } from "../public/utils/common";
+
+describe("The Keys client should set the serviceVersion", () => {
+  const keyVaultUrl = `https://keyvaultname.vault.azure.net`;
+
+  const mockHttpClient: HttpClient = {
+    async sendRequest(request: PipelineRequest): Promise<PipelineResponse> {
+      return {
+        status: 200,
+        headers: createHttpHeaders(),
+        request: request,
+        bodyAsText: JSON.stringify({
+          key: {
+            kid: `${keyVaultUrl}/keys/keyName/id`,
+          },
+        }),
+      };
+    },
+  };
+
+  let sandbox: SinonSandbox;
+  let spy: SinonSpy<[PipelineRequest], Promise<PipelineResponse>>;
+  let credential: ClientSecretCredential;
+  beforeEach(async () => {
+    sandbox = createSandbox();
+    spy = sandbox.spy(mockHttpClient, "sendRequest");
+
+    credential = new ClientSecretCredential("tenant", "client", "secret");
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it("it should default to the latest API version", async function () {
+    const client = new KeyClient(keyVaultUrl, credential, {
+      httpClient: mockHttpClient,
+    });
+    await client.createKey("keyName", "RSA");
+
+    const calls = spy.getCalls();
+    assert.equal(
+      calls[0].args[0].url,
+      `https://keyvaultname.vault.azure.net/keys/keyName/create?api-version=${LATEST_API_VERSION}`
+    );
+  });
+
+  versionsToTest(serviceVersions, {}, (serviceVersion) => {
+    it("it should allow us to specify an API version from a specific set of versions", async function () {
+      const client = new KeyClient(keyVaultUrl, credential, {
+        serviceVersion: serviceVersion,
+        httpClient: mockHttpClient,
+      });
+      await client.createKey("keyName", "RSA");
+
+      const calls = spy.getCalls();
+      const lastCall = calls[calls.length - 1];
+      assert.equal(
+        lastCall.args[0].url,
+        `https://keyvaultname.vault.azure.net/keys/keyName/create?api-version=${serviceVersion}`
+      );
+    });
+  });
+});
---
+++ sdk/synapse/synapse-access-control/test/public/utils/recordedClient.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import "./env";
+
+import { Context } from "mocha";
+import { createTestCredential } from "@azure-tools/test-credential";
+import { AccessControlClient, AccessControlClientOptionalParams } from "../../../src";
+import {
+  Recorder,
+  RecorderStartOptions,
+  env,
+} from "@azure-tools/test-recorder";
+
+export function createClient(options?: AccessControlClientOptionalParams): AccessControlClient {
+  let credential = createTestCredential();
+  return new AccessControlClient(credential, env.ENDPOINT as string, { ...options });
+}
+
+/**
+ * creates the recorder and reads the environment variables from the `.env` file.
+ * Should be called first in the test suite to make sure environment variables are
+ * read before they are being used.
+ */
+export async function createRecorder(context: Context): Promise<Recorder> {
+  const recorderStartOptions: RecorderStartOptions = {
+    envSetupForPlayback: {
+      AZURE_CLIENT_ID: "azure_client_id",
+      AZURE_CLIENT_SECRET: "azure_client_secret",
+      AZURE_TENANT_ID: "88888888-8888-8888-8888-888888888888",
+      ENDPOINT: "https://testaccount.dev.azuresynapse.net"
+    },
+  };
+  const recorder = new Recorder(context.currentTest);
+  await recorder.start(recorderStartOptions);
+  return recorder;
+}
