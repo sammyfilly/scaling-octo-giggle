3015ce27466dd45589f84ca8647b49f84cf25f8d
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:39:37 +0100

    Initialize jpeng-ms-patch-1

---
+++ sdk/formrecognizer/ai-form-recognizer/src/options/index.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { AnalyzeDocumentOptions } from "./AnalyzeDocumentsOptions";
+import {
+  CreateDocumentModelOptions,
+  CommonModelCreationOptions,
+  DocumentModelBuildMode,
+  BeginBuildDocumentModelOptions,
+  BeginComposeDocumentModelOptions,
+} from "./BuildModelOptions";
+import { BeginCopyModelOptions } from "./BeginCopyModelOptions";
+import { DeleteDocumentModelOptions } from "./DeleteModelOptions";
+import {
+  DocumentAnalysisClientOptions,
+  DocumentModelAdministrationClientOptions,
+  FormRecognizerApiVersion,
+  FormRecognizerCommonClientOptions,
+  StringIndexType,
+} from "./FormRecognizerClientOptions";
+import { GetCopyAuthorizationOptions } from "./GetCopyAuthorizationOptions";
+import { GetResourceDetailsOptions } from "./GetResourceDetailsOptions";
+import { GetModelOptions } from "./GetModelOptions";
+import { GetOperationOptions } from "./GetOperationOptions";
+import { ListModelsOptions } from "./ListModelsOptions";
+import { ListOperationsOptions } from "./ListOperationsOptions";
+import { PollerOptions } from "./PollerOptions";
+
+export {
+  AnalyzeDocumentOptions,
+  CreateDocumentModelOptions,
+  BeginBuildDocumentModelOptions,
+  BeginComposeDocumentModelOptions,
+  CommonModelCreationOptions,
+  BeginCopyModelOptions,
+  DocumentModelBuildMode,
+  FormRecognizerCommonClientOptions,
+  DocumentAnalysisClientOptions,
+  DocumentModelAdministrationClientOptions,
+  GetCopyAuthorizationOptions,
+  GetResourceDetailsOptions,
+  GetModelOptions,
+  GetOperationOptions,
+  DeleteDocumentModelOptions,
+  ListModelsOptions,
+  ListOperationsOptions,
+  PollerOptions,
+  StringIndexType,
+  FormRecognizerApiVersion,
+};
