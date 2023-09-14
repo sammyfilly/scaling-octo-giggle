3c7a5ed9e966961d13420e83301268a13a92049a
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:34:48 +0100

    Initialize vikas/pipeline-fix-gateway

---
+++ sdk/search/search-documents/test/internal/base64.spec.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { decode, encode } from "../../src/base64";
+
+describe("base64", function () {
+  it("strings can roundtrip", function () {
+    const message = "Only *you* can prevent null dereferences!";
+    const encoded = encode(message);
+    const decoded = decode(encoded);
+    assert.strictEqual(decoded, message);
+  });
+});
---
+++ sdk/tables/data-tables/test/internal/odata.spec.ts
@@ -0,0 +1,47 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { odata } from "../../src";
+
+describe("odata", function () {
+  it("should handle empty string", function () {
+    const testString = odata``;
+    assert.equal(testString, "");
+  });
+
+  it("should handle string without variables", function () {
+    const testString = odata`Foo`;
+    assert.equal(testString, "Foo");
+  });
+
+  it("should not quote number", function () {
+    const testValue = 4;
+    const testString = odata`test number ${testValue}`;
+    assert.equal(testString, "test number 4");
+  });
+
+  it("should not quote boolean", function () {
+    const testValue = true;
+    const testString = odata`test boolean ${testValue}`;
+    assert.equal(testString, "test boolean true");
+  });
+
+  it("should quote string", function () {
+    const testValue = "FooBar";
+    const testString = odata`test string ${testValue}`;
+    assert.equal(testString, "test string 'FooBar'");
+  });
+
+  it("should escape string with quotes", function () {
+    const testValue = '"FooBar"';
+    const testString = odata`test string ${testValue}`;
+    assert.equal(testString, "test string '\"FooBar\"'");
+  });
+
+  it("should not escape property names unnecessarily", function () {
+    const testDate = new Date(1);
+    const testString = odata`test lt ${testDate}`;
+    assert.equal(testString, `test lt datetime'${testDate.toISOString()}'`);
+  });
+});
