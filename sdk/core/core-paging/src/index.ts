51c679539bbe3c1805050b9a452fb88f2a599bbb
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 05:35:45 +0100

    Initialize dw/test-apiview

---
+++ sdk/core/core-paging/src/index.ts
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import "@azure/core-asynciterator-polyfill";
+
+export * from "./models";
+export * from "./getPagedAsyncIterator";
