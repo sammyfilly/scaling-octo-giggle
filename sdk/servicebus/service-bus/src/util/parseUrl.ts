73ca852220b95d433db16cb9351f19affba455f0
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:41:26 +0100

    Initialize joheredi-multi-version-design

---
+++ sdk/servicebus/service-bus/src/util/parseUrl.ts
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * @internal
+ */
+import { URL } from "url";
+
+/**
+ * @internal
+ */
+export const parseURL = (rawUrl: string): URL => {
+  return new URL(rawUrl);
+};
---
+++ sdk/textanalytics/ai-text-analytics/test/public/utils/resultHelper.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { isRestError } from "@azure/core-rest-pipeline";
+import { assert } from "chai";
+
+import { TextAnalyticsErrorResult, TextAnalyticsSuccessResult } from "../../../src/";
+
+export function assertAllSuccess<TSuccess extends TextAnalyticsSuccessResult>(
+  results: (TextAnalyticsErrorResult | TSuccess)[]
+): void {
+  for (const result of results) {
+    assert.ok(isSuccess(result));
+  }
+}
+
+export function isSuccess<TSuccess extends TextAnalyticsSuccessResult>(
+  res: TextAnalyticsErrorResult | TSuccess
+): res is TSuccess {
+  return res.error === undefined;
+}
+
+export async function assertRestError(
+  error: Promise<unknown>,
+  options: {
+    statusCode?: number;
+    code?: string;
+    messagePattern?: RegExp;
+  } = {}
+): Promise<void> {
+  const { code, statusCode, messagePattern } = options;
+  try {
+    await error;
+    assert.fail(`Should have failed instead!`);
+  } catch (e: unknown) {
+    if (isRestError(e)) {
+      if (code) {
+        assert.equal(e.code, code);
+      }
+      if (statusCode) {
+        assert.equal(e.statusCode, statusCode);
+      }
+      if (messagePattern) {
+        assert.match(e.message, messagePattern);
+      }
+    } else {
+      assert.fail(`Unexpected error: ${JSON.stringify(e)}`);
+    }
+  }
+}
