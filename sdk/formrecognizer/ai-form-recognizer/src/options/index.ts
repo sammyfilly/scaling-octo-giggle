174f0444f336aeafbee12a5d0519f2885b0013f1
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 05:45:37 +0100

    Initialize feature/communication-email

---
+++ sdk/formrecognizer/ai-form-recognizer/src/options/index.ts
@@ -0,0 +1,50 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { AnalyzeDocumentOptions } from "./AnalyzeDocumentsOptions";
+import {
+  CreateModelOptions,
+  CommonModelCreationOptions,
+  DocumentModelBuildMode,
+  BeginBuildModelOptions,
+  BeginComposeModelOptions,
+} from "./BuildModelOptions";
+import { BeginCopyModelOptions } from "./BeginCopyModelOptions";
+import { DeleteModelOptions } from "./DeleteModelOptions";
+import {
+  DocumentAnalysisClientOptions,
+  DocumentModelAdministrationClientOptions,
+  FormRecognizerApiVersion,
+  FormRecognizerCommonClientOptions,
+  StringIndexType,
+} from "./FormRecognizerClientOptions";
+import { GetCopyAuthorizationOptions } from "./GetCopyAuthorizationOptions";
+import { GetResourceDetailsOptions } from "./GetResourceDetailsOptions";
+import { GetModelOptions } from "./GetModelOptions";
+import { GetOperationOptions } from "./GetOperationOptions";
+import { ListModelsOptions } from "./ListModelsOptions";
+import { ListOperationsOptions } from "./ListOperationsOptions";
+import { PollerOptions } from "./PollerOptions";
+
+export {
+  AnalyzeDocumentOptions,
+  CreateModelOptions,
+  BeginBuildModelOptions,
+  BeginComposeModelOptions,
+  CommonModelCreationOptions,
+  BeginCopyModelOptions,
+  DocumentModelBuildMode,
+  FormRecognizerCommonClientOptions,
+  DocumentAnalysisClientOptions,
+  DocumentModelAdministrationClientOptions,
+  GetCopyAuthorizationOptions,
+  GetResourceDetailsOptions,
+  GetModelOptions,
+  GetOperationOptions,
+  DeleteModelOptions,
+  ListModelsOptions,
+  ListOperationsOptions,
+  PollerOptions,
+  StringIndexType,
+  FormRecognizerApiVersion,
+};
