f857e01eaeae00a9e6f724a99406b49c1f4128ba
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:40:50 +0100

    Initialize sync-eng/common-benbp/prepare-sparse-5947-ForTestPipeline

---
+++ sdk/purview/purview-scanning-rest/src/index.ts
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import PurviewScanning from "./purviewScanning";
+
+export * from "./purviewScanning";
+export * from "./models";
+export * from "./parameters";
+export * from "./responses";
+export * from "./paginateHelper";
+
+export default PurviewScanning;
