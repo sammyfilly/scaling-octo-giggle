49c28084e50721759724a874bc21efa097743b38
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 05:53:15 +0100

    Initialize hotfix/core-http_2.3.1

---
+++ sdk/schemaregistry/schema-registry/src/conversions.ts
@@ -0,0 +1,71 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { Schema, SchemaProperties } from "./models";
+
+import {
+  SchemaGetByIdResponse,
+  SchemaQueryIdByContentResponse as SchemaQueryIdByDefinitionResponse,
+  SchemaRegisterResponse,
+} from "./generated/models";
+import { getSchemaDefinition } from "./getSchemaDefinition";
+
+/**
+ * Union of generated client's responses that return schema definition.
+ */
+type GeneratedSchemaResponse = SchemaGetByIdResponse;
+
+/**
+ * Union of generated client's responses that return schema ID.
+ */
+type GeneratedSchemaIdResponse = SchemaRegisterResponse | SchemaQueryIdByDefinitionResponse;
+
+/**
+ * Converts generated client's response to IdentifiedSchemaResponse.
+ *
+ * @internal
+ */
+export async function convertSchemaResponse(response: GeneratedSchemaResponse): Promise<Schema> {
+  const schemaDefinition = await getSchemaDefinition(response);
+  return {
+    definition: schemaDefinition,
+    properties: {
+      id: response.schemaId!,
+      format: mapContentTypeToFormat(response.contentType!),
+      groupName: response.schemaGroupName!,
+      name: response.schemaName!,
+      version: response.schemaVersion!,
+    },
+  };
+}
+
+/**
+ * Converts generated client's response to SchemaIdentityResponse.
+ *
+ * @internal
+ */
+export function convertSchemaIdResponse(
+  schemaFormat: string
+): (response: GeneratedSchemaIdResponse) => SchemaProperties {
+  return (response: GeneratedSchemaIdResponse): SchemaProperties => {
+    return {
+      // `!`s here because server is required to return these on success, but that
+      // is not modeled by the generated client.
+      id: response.schemaId!,
+      format: schemaFormat,
+      groupName: response.schemaGroupName!,
+      name: response.schemaName!,
+      version: response.schemaVersion!,
+    };
+  };
+}
+
+function mapContentTypeToFormat(contentType: string): string {
+  const parts = /.*serialization=(.*)$/.exec(contentType);
+  const schemaFormat = parts?.[1];
+  if (schemaFormat) {
+    return schemaFormat;
+  } else {
+    throw new Error(`Unrecognized response's content-type: ${contentType}`);
+  }
+}
