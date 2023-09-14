75f09a8e46ada02a5f52d85a0629a4e0f09c57c3
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 05:55:38 +0100

    Initialize sdkGen/confidentialledger-js-d9bac7ea-8870-ed11-9562-000d3a5651e3/dpg-confidentialledger-@azure-rest/azure-confidentialledger-bugbash-rest

---
+++ sdk/confidentialledger/confidential-ledger-rest/src/confidentialLedger.ts
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { TokenCredential, isTokenCredential } from "@azure/core-auth";
+
+import { ClientOptions } from "@azure-rest/core-client";
+import { ConfidentialLedgerClient } from "./generated/src/clientDefinitions";
+import GeneratedConfidentialLedger from "./generated/src/confidentialLedger";
+
+export default function ConfidentialLedger(
+  ledgerEndpoint: string,
+  ledgerIdentityCertificate: string,
+  options?: ClientOptions
+): ConfidentialLedgerClient;
+export default function ConfidentialLedger(
+  ledgerEndpoint: string,
+  ledgerIdentityCertificate: string,
+  credentials: TokenCredential,
+  options?: ClientOptions
+): ConfidentialLedgerClient;
+export default function ConfidentialLedger(
+  ledgerEndpoint: string,
+  ledgerIdentityCertificate: string,
+  credentialsOrOptions?: TokenCredential | ClientOptions,
+  opts?: ClientOptions
+): ConfidentialLedgerClient {
+  let credentials: TokenCredential | undefined;
+  let options: ClientOptions;
+
+  if (isTokenCredential(credentialsOrOptions)) {
+    credentials = credentialsOrOptions;
+    options = opts ?? {};
+  } else {
+    options = credentialsOrOptions ?? {};
+  }
+
+  const tlsOptions = options?.tlsOptions ?? {};
+  tlsOptions.ca = ledgerIdentityCertificate;
+  const confidentialLedger = GeneratedConfidentialLedger(ledgerEndpoint, credentials!, {
+    ...options,
+    tlsOptions,
+  });
+  return confidentialLedger;
+}
