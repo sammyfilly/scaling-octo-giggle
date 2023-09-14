f24018238574c73b27ea5dbf4711ae70426db1ac
Sammy filly <136061549+sammyfilly@users.noreply.github.com>
2023-09-14 06:52:41 +0100

    Initialize sync-eng/common-djurek/fix-service-level-readme-automation-6281

---
+++ sdk/identity/identity/test/internal/utils.spec.ts
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { assert } from "chai";
+import { getAuthority } from "../../src/msal/utils";
+import { processMultiTenantRequest } from "../../src/util/tenantIdUtils";
+
+describe("Identity utilities", function () {
+  describe("validateMultiTenantRequest", function () {
+    it("returns the original tenant if getTokenOptions does not have a tenantId", async function () {
+      const originalTenant = "credential-options-tenant-id";
+      const resultingTenant = processMultiTenantRequest(originalTenant);
+      assert.equal(resultingTenant, originalTenant);
+    });
+
+    it("it shouldn't throw if the tenant received is the same as the tenant we already had", async function () {
+      assert.equal(
+        processMultiTenantRequest("same-tenant", {
+          tenantId: "same-tenant",
+        }),
+        "same-tenant"
+      );
+    });
+
+    it("should pick the tenant from the options if has allowed all tenants", async function () {
+      assert.equal(
+        processMultiTenantRequest(
+          "credential-options-tenant-id",
+          {
+            tenantId: "get-token-options-tenant-id",
+          },
+          ["*"]
+        ),
+        "get-token-options-tenant-id"
+      );
+    });
+
+    it("should pick the tenant from the options if has allowed the options tenant", async function () {
+      assert.equal(
+        processMultiTenantRequest(
+          "credential-options-tenant-id",
+          {
+            tenantId: "get-token-options-tenant-id",
+          },
+          ["get-token-options-tenant-id"]
+        ),
+        "get-token-options-tenant-id"
+      );
+    });
+  });
+
+  describe("getAuthority", () => {
+    it("should add the tenant Id when the authority host ends with a slash", async function () {
+      assert.equal(
+        getAuthority("tenant-id", "https://login.microsoftonline.com/"),
+        "https://login.microsoftonline.com/tenant-id"
+      );
+    });
+    it("should add the tenant Id when the authority host ends without a slash", async function () {
+      assert.equal(
+        getAuthority("tenant-id", "https://login.microsoftonline.com"),
+        "https://login.microsoftonline.com/tenant-id"
+      );
+    });
+    it("should not add the tenant twice", async function () {
+      assert.equal(
+        getAuthority("tenant-id", "https://login.microsoftonline.com/tenant-id"),
+        "https://login.microsoftonline.com/tenant-id"
+      );
+    });
+    it("should not add the tenant twice even when it ends in a slash", async function () {
+      assert.equal(
+        getAuthority("tenant-id", "https://login.microsoftonline.com/tenant-id/"),
+        "https://login.microsoftonline.com/tenant-id/"
+      );
+    });
+  });
+});
---
+++ sdk/textanalytics/ai-text-analytics/src/index.ts
@@ -0,0 +1,152 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+export { AzureKeyCredential } from "@azure/core-auth";
+
+export {
+  TextAnalyticsClient,
+  TextAnalyticsClientOptions,
+  DetectLanguageOptions,
+  RecognizeCategorizedEntitiesOptions,
+  AnalyzeSentimentOptions,
+  ExtractKeyPhrasesOptions,
+  RecognizePiiEntitiesOptions,
+  RecognizeLinkedEntitiesOptions,
+  PiiEntityDomain,
+  TextAnalyticsActions,
+  RecognizeCategorizedEntitiesAction,
+  RecognizePiiEntitiesAction,
+  ExtractKeyPhrasesAction,
+  BeginAnalyzeActionsOptions,
+  AnalyzeActionsPollerLike,
+  BeginAnalyzeHealthcareEntitiesOptions,
+  AnalyzeHealthcareEntitiesPollerLike,
+  AnalyzeActionsOperationState,
+  AnalyzeHealthcareOperationState,
+  AnalysisPollOperationState,
+  OperationMetadata,
+  AnalyzeActionsOperationMetadata,
+  StringIndexType,
+  RecognizeLinkedEntitiesAction,
+  AnalyzeSentimentAction,
+  PollerLikeWithCancellation,
+} from "./textAnalyticsClient";
+export { TextAnalyticsOperationOptions } from "./textAnalyticsOperationOptions";
+export {
+  DetectLanguageResult,
+  DetectLanguageErrorResult,
+  DetectLanguageSuccessResult,
+} from "./detectLanguageResult";
+export { DetectLanguageResultArray } from "./detectLanguageResultArray";
+export {
+  CategorizedEntity,
+  RecognizeCategorizedEntitiesResult,
+  RecognizeCategorizedEntitiesErrorResult,
+  RecognizeCategorizedEntitiesSuccessResult,
+} from "./recognizeCategorizedEntitiesResult";
+export {
+  PiiEntity,
+  RecognizePiiEntitiesResult,
+  RecognizePiiEntitiesErrorResult,
+  RecognizePiiEntitiesSuccessResult,
+} from "./recognizePiiEntitiesResult";
+export { RecognizePiiEntitiesResultArray } from "./recognizePiiEntitiesResultArray";
+export { RecognizeCategorizedEntitiesResultArray } from "./recognizeCategorizedEntitiesResultArray";
+export {
+  AnalyzeSentimentResult,
+  AnalyzeSentimentErrorResult,
+  AnalyzeSentimentSuccessResult,
+  SentenceSentiment,
+  Opinion,
+  TargetSentiment,
+  AssessmentSentiment,
+} from "./analyzeSentimentResult";
+export { AnalyzeSentimentResultArray } from "./analyzeSentimentResultArray";
+export {
+  ExtractKeyPhrasesResult,
+  ExtractKeyPhrasesErrorResult,
+  ExtractKeyPhrasesSuccessResult,
+} from "./extractKeyPhrasesResult";
+export { ExtractKeyPhrasesResultArray } from "./extractKeyPhrasesResultArray";
+export {
+  RecognizeLinkedEntitiesResult,
+  RecognizeLinkedEntitiesErrorResult,
+  RecognizeLinkedEntitiesSuccessResult,
+} from "./recognizeLinkedEntitiesResult";
+export { RecognizeLinkedEntitiesResultArray } from "./recognizeLinkedEntitiesResultArray";
+export {
+  PagedAnalyzeHealthcareEntitiesResult,
+  PagedAsyncIterableAnalyzeHealthcareEntitiesResult,
+  AnalyzeHealthcareEntitiesResultArray,
+  AnalyzeHealthcareEntitiesResult,
+  AnalyzeHealthcareEntitiesSuccessResult,
+  AnalyzeHealthcareEntitiesErrorResult,
+  HealthcareEntity,
+  EntityDataSource,
+  HealthcareEntityRelation,
+  HealthcareEntityRelationRole,
+  HealthcareEntityRelationRoleType,
+} from "./analyzeHealthcareEntitiesResult";
+export {
+  PagedAnalyzeActionsResult,
+  PagedAsyncIterableAnalyzeActionsResult,
+  AnalyzeActionsResult,
+  RecognizeCategorizedEntitiesActionResult,
+  RecognizePiiEntitiesActionResult,
+  ExtractKeyPhrasesActionResult,
+  TextAnalyticsActionSuccessState,
+  TextAnalyticsActionErrorResult,
+  RecognizeCategorizedEntitiesActionErrorResult,
+  RecognizeCategorizedEntitiesActionSuccessResult,
+  RecognizePiiEntitiesActionErrorResult,
+  RecognizePiiEntitiesActionSuccessResult,
+  ExtractKeyPhrasesActionErrorResult,
+  ExtractKeyPhrasesActionSuccessResult,
+  RecognizeLinkedEntitiesActionResult,
+  RecognizeLinkedEntitiesActionSuccessResult,
+  RecognizeLinkedEntitiesActionErrorResult,
+  AnalyzeSentimentActionErrorResult,
+  AnalyzeSentimentActionResult,
+  AnalyzeSentimentActionSuccessResult,
+} from "./analyzeActionsResult";
+export {
+  ErrorCode,
+  TextAnalyticsError,
+  TextAnalyticsErrorResult,
+  TextAnalyticsSuccessResult,
+} from "./textAnalyticsResult";
+
+export { TextAnalyticsAction } from "./textAnalyticsAction";
+
+// Models
+export {
+  DetectedLanguage,
+  TextDocumentStatistics,
+  SentimentConfidenceScores,
+  TextDocumentInput,
+  DetectLanguageInput,
+  TextDocumentBatchStatistics,
+  Entity,
+  DocumentSentimentLabel,
+  SentenceSentimentLabel,
+  ErrorCodeValue,
+  InnerErrorCodeValue,
+  KnownInnerErrorCodeValue,
+  WarningCode,
+  KnownWarningCode,
+  LinkedEntity,
+  Match,
+  SentenceAssessment,
+  TargetConfidenceScoreLabel,
+  TokenSentimentValue,
+  TextAnalyticsWarning,
+  State as TextAnalyticsOperationStatus,
+  HealthcareAssertion as EntityAssertion,
+  PiiCategory as PiiEntityCategory,
+  Association as EntityAssociation,
+  Certainty as EntityCertainty,
+  Conditionality as EntityConditionality,
+  RelationType as HealthcareEntityRelationType,
+  KnownHealthcareEntityCategory,
+  HealthcareEntityCategory,
+} from "./generated/models";
